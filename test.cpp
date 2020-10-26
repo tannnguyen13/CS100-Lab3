cd290af480d0a13cc540fc03c8c681a02c2f89fe
tannnguyen13 <tann2000@gmail.com>
2020-10-26 12:15:23 -0700

    Added the repository with previous lab files from Lab-01.

---
+++ test.cpp
@@ -0,0 +1,45 @@
+#include "c-echo.h"
+
+#include "gtest/gtest.h"
+
+TEST(EchoTest, HelloWorld) {
+	char* test_val[3]; 
+	test_val[0] = "./c-echo"; 
+	test_val[1] = "hello"; 
+	test_val[2] = "world";
+    	EXPECT_EQ("hello world", echo(3,test_val));
+}
+
+TEST(EchoTest, EmptyString) {
+	char* test_val[1]; 
+	test_val[0] = "./c-echo";
+    	EXPECT_EQ("", echo(1,test_val));
+}
+
+TEST(EchoTest, OneCharacter) {
+	char* test_val[2];
+	test_val[0] = "./c-echo";
+	test_val[1] = "a";
+	EXPECT_EQ("a", echo(2, test_val));
+}
+
+TEST(EchoTest, OneInteger) {
+	char *test_val[2];
+	test_val[0] = "./c-echo";
+	test_val[1] = "1";
+	EXPECT_EQ("1", echo(2, test_val));
+}
+
+TEST(EchoTest, Space) {
+	char *test_val[2];
+	test_val[0] = "./c-echo";
+	test_val[1] = " ";
+	EXPECT_EQ(" ", echo(2, test_val));
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
+
